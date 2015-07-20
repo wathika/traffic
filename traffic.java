ef68986f5524bbacc4875461a4c4838db7fad595
viscount <vcwathika@gmail.com>
2015-07-20 16:20:54 +0300

    Modified Read me

--- traffic.java
+++ traffic.java
@@ -2,7 +2,26 @@ package p3count;
 import java.util.Scanner;
 public class traffic {
 	public static void main(String arg[]){
-		System.out.println("Hi there, which road would you like to use today?\n1:Ngong Road\n2 Waiyaki Way\n3:Mombasa road\n4:Mbagathi way\n5:Jogoo Road");
+		System.out.println("Hey my name is autotraffic3000, and i can serve you with information about the traffic status in NBI.\nEnter 1:Roads with or without traffic\nEnter 2:specific roads");{
+		Scanner ans = new Scanner(System.in);
+		int jibu = ans.nextInt();
+		if(jibu == 1){
+			System.out.println("Enter 1 to check roads with traffic\nEnter 2 to check roads without traffic");
+			Scanner tr = new Scanner(System.in);
+			int w = tr.nextInt();
+			if(w == 1){
+				System.out.println("Roads with traffic are:\nNgong' road\nMombasa road\nJogoo road");
+			}
+			else if(w == 2){
+				System.out.println("Roads without traffic are:\nWaiyaki way\nMbagathi way");
+			}
+		
+		
+		
+		}
+		else{
+		
+		System.out.println("Which road would you like to use?\n1:Ngong Road\n2 Waiyaki Way\n3:Mombasa road\n4:Mbagathi way\n5:Jogoo Road");
 	Scanner num = new Scanner(System.in);
 	System.out.println("Please enter road number:");
     int x = num.nextInt();
@@ -22,5 +41,5 @@ public class traffic {
 		System.out.println("There's traffic on Jogoo Road :(, you can pass through ....");
        }
 	}
-	}
-
+	}}
+}
